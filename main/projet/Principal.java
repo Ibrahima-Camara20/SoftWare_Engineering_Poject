638abea9d68a4f98912f104c9aaae3798b6b2054
camar <ibrahima.camara@etu.univ-cotedazur.fr>
2025-09-17 12:24:57 +0200

    Ajout de la Classe EmpaquetAbstrait et EmpaquetCroise

--- main/projet/Principal.java
+++ main/projet/Principal.java
@@ -1,12 +1,21 @@
-import util.BitsUtil;
 
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Projet Bit Packing démarré !");
 
-        
-        System.out.println("bitsNecessaires(5) = " + BitsUtil.bitsNecessaires(5));       
-        System.out.println("bitsNecessaires(1024) = " + BitsUtil.bitsNecessaires(1024)); 
-        System.out.println("bitsNecessaires(0) = " + BitsUtil.bitsNecessaires(0));       
+        int[] donnees = {10, 20, 30, 40, 50};
+
+        EmpaquetBits empaqueteur = new EmpaquetCroise(false);
+        empaqueteur.compresser(donnees);
+
+        int[] sortie = new int[donnees.length];
+        empaqueteur.decompresser(sortie);
+
+        System.out.println("Test obtenir(2) = " + empaqueteur.obtenir(2)); // doit afficher 30
+        System.out.print("Décompression brute : ");
+        for (int v : sortie) {
+            System.out.print(v + " ");
+        }
+        System.out.println();
     }
 }
