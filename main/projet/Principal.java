74e09b0295f337e626b3e906415cb3c85c20361c
camar <ibrahima.camara@etu.univ-cotedazur.fr>
2025-09-18 15:48:14 +0200

    Ajout de la classe EmpaquetNonCroise

--- main/projet/Principal.java
+++ main/projet/Principal.java
@@ -5,17 +5,24 @@ public class Principal {
 
         int[] donnees = {10, 20, 30, 40, 50};
 
-        EmpaquetBits empaqueteur = new EmpaquetCroise(false);
-        empaqueteur.compresser(donnees);
+        // --- Version croisée ---
+        EmpaquetBits croise = new EmpaquetCroise(false);
+        croise.compresser(donnees);
+        int[] sortie1 = new int[donnees.length];
+        croise.decompresser(sortie1);
+        System.out.println("Croisé obtenir(2) = " + croise.obtenir(2));
+        System.out.print("Croisé décompression : ");
+        for (int v : sortie1) System.out.print(v + " ");
+        System.out.println();
 
-        int[] sortie = new int[donnees.length];
-        empaqueteur.decompresser(sortie);
-
-        System.out.println("Test obtenir(2) = " + empaqueteur.obtenir(2)); // doit afficher 30
-        System.out.print("Décompression brute : ");
-        for (int v : sortie) {
-            System.out.print(v + " ");
-        }
+        // --- Version non croisée ---
+        EmpaquetBits nonCroise = new EmpaquetNonCroise(false);
+        nonCroise.compresser(donnees);
+        int[] sortie2 = new int[donnees.length];
+        nonCroise.decompresser(sortie2);
+        System.out.println("Non croisé obtenir(2) = " + nonCroise.obtenir(2));
+        System.out.print("Non croisé décompression : ");
+        for (int v : sortie2) System.out.print(v + " ");
         System.out.println();
     }
 }
