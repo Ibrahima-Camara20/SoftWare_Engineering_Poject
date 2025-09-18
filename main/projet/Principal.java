2416c6739d1d17d535aed29cc9990e825ba1be97
camar <ibrahima.camara@etu.univ-cotedazur.fr>
2025-09-18 16:08:09 +0200

    Ajout de la classe FabriqueEmpaquet

--- main/projet/Principal.java
+++ main/projet/Principal.java
@@ -5,24 +5,15 @@ public class Principal {
 
         int[] donnees = {10, 20, 30, 40, 50};
 
-        // --- Version croisée ---
-        EmpaquetBits croise = new EmpaquetCroise(false);
+      
+  
+        EmpaquetBits croise = FabriqueEmpaquet.creer("croise", false);
         croise.compresser(donnees);
-        int[] sortie1 = new int[donnees.length];
-        croise.decompresser(sortie1);
         System.out.println("Croisé obtenir(2) = " + croise.obtenir(2));
-        System.out.print("Croisé décompression : ");
-        for (int v : sortie1) System.out.print(v + " ");
-        System.out.println();
 
-        // --- Version non croisée ---
-        EmpaquetBits nonCroise = new EmpaquetNonCroise(false);
+        
+        EmpaquetBits nonCroise = FabriqueEmpaquet.creer("noncroise", false);
         nonCroise.compresser(donnees);
-        int[] sortie2 = new int[donnees.length];
-        nonCroise.decompresser(sortie2);
         System.out.println("Non croisé obtenir(2) = " + nonCroise.obtenir(2));
-        System.out.print("Non croisé décompression : ");
-        for (int v : sortie2) System.out.print(v + " ");
-        System.out.println();
     }
 }
