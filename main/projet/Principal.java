0f8cdeb04845f3fa36f13f297d1da49ea7cd275f
camar <ibrahima.camara@etu.univ-cotedazur.fr>
2025-09-18 16:57:21 +0200

    Ajout de la classe Mesure

--- main/projet/Principal.java
+++ main/projet/Principal.java
@@ -1,19 +1,12 @@
-
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Projet Bit Packing démarré !");
 
+       
         int[] donnees = {10, 20, 30, 40, 50};
 
-      
-  
-        EmpaquetBits croise = FabriqueEmpaquet.creer("croise", false);
-        croise.compresser(donnees);
-        System.out.println("Croisé obtenir(2) = " + croise.obtenir(2));
-
         
-        EmpaquetBits nonCroise = FabriqueEmpaquet.creer("noncroise", false);
-        nonCroise.compresser(donnees);
-        System.out.println("Non croisé obtenir(2) = " + nonCroise.obtenir(2));
+        Mesure.executer(donnees, "croise", false);
+        Mesure.executer(donnees, "noncroise", false);
     }
 }
